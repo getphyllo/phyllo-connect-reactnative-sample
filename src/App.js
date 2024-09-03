370bd2985c0bd8467a12ad3e47b6a2e59a37afe7
Pankaj Patel <patelpankaj.1010@gmail.com>
2024-09-03 17:42:18 +0530

    Multiple Callback Fixes

--- src/App.js
+++ src/App.js
@@ -1,87 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet, TouchableOpacity, Alert, Text } from "react-native";
-import PhylloConnect from "react-native-phyllo-connect";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import BouncyCheckbox from "react-native-bouncy-checkbox";
+import React, { useEffect, useState } from 'react'
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  Text,
+  NativeEventEmitter,
+} from 'react-native'
+import PhylloConnect from 'react-native-phyllo-connect'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import BouncyCheckbox from 'react-native-bouncy-checkbox'
 
-import { createUser, createUserToken } from "./APIHandler";
-import { generateRandomString } from "./randomGenerator";
-import clientConfig from "./config";
-import Toast from "react-native-simple-toast";
+import { createUser, createUserToken } from './APIHandler'
+import { generateRandomString } from './randomGenerator'
+import clientConfig from './config'
+import Toast from 'react-native-simple-toast'
 
 export default function ExampleApp() {
-  const [existingUser, setExistingUser] = useState(false);
-  const [userId, setUserId] = useState("");
-  const [userToken, setUserToken] = useState("");
+  const [existingUser, setExistingUser] = useState(false)
+  const [userId, setUserId] = useState('')
+  const [userToken, setUserToken] = useState('')
 
   useEffect(() => {
     const getUserFromStorage = async () => {
-      const userId = await AsyncStorage.getItem("user-id");
-      const token = await AsyncStorage.getItem("user-token");
+      const userId = await AsyncStorage.getItem('user-id')
+      const token = await AsyncStorage.getItem('user-token')
 
       if (!userId || !token) {
-        return;
+        return
       }
-      setUserId(userId);
-      setUserToken(token);
-    };
+      setUserId(userId)
+      setUserToken(token)
+    }
 
-    getUserFromStorage();
-  }, []);
-
-  // A callback function called upon event
-  const onExitCallBack = (reason, userId) => {
-    console.log(`onExit reason: ${reason}, userId: ${userId}`);
-    Toast.show(`onExit reason: ${reason}, userId: ${userId}`);
-  };
-  const onAccountConnectedCallBack = (accountId, workplatformId, userId) => {
-    console.log(
-      `onAccountConnected accountId: ${accountId}, workplatformId: ${workplatformId}, userId: ${userId}`
-    );
-    Toast.show(
-      `onAccountConnected accountId: ${accountId}, workplatformId: ${workplatformId}, userId: ${userId}`
-    );
-  };
-  const onAccountDisconnectedCallBack = (accountId, workplatformId, userId) => {
-    console.log(
-      `onAccountDisconnected accountId: ${accountId}, workplatformId: ${workplatformId}, userId: ${userId}`
-    );
-    Toast.show(
-      `onAccountDisconnected accountId: ${accountId}, workplatformId: ${workplatformId}, userId: ${userId}`
-    );
-  };
-  const onTokenExpiredCallBack = (userId) => {
-    console.log(`onTokenExpired userId: ${userId}`);
-    Toast.show(`onTokenExpired userId: ${userId}`);
-  };
-
-  const onConnectionFailure = (reason, workplatformId, userId) => {
-    console.log(
-      `onConnectionFailure reason: ${reason}, workplatformId: ${workplatformId}, userId: ${userId}`
-    );
-    Toast.show(
-      `onConnectionFailure reason: ${reason}, workplatformId: ${workplatformId}, userId: ${userId}`
-    );
-  };
+    getUserFromStorage()
+  }, [])
 
   const onPressButton = async (workPlatformId) => {
-    const clientDisplayName = "Phyllo Sample";
-    const externalId = generateRandomString(20);
-    const environment = clientConfig.env;
-    const baseURL = await PhylloConnect.getEnvBaseURl(environment.toString());
-    let id, token;
+    const clientDisplayName = 'Example'
+    const externalId = generateRandomString(20)
+    const environment = clientConfig.env
+    const baseURL = await PhylloConnect.getPhylloEnvironmentUrl('sandbox')
+    console.log('baseURL', baseURL)
+    let id, token
     try {
       // Create a user, SDK Token if the user is new user
       if (existingUser) {
-        id = userId;
-        token = userToken;
+        id = userId
+        token = userToken
       } else {
-        id = await createUser(generateRandomString(8), externalId, baseURL);
-        token = await createUserToken(id, baseURL);
-        await AsyncStorage.setItem("user-id", id);
-        await AsyncStorage.setItem("user-token", token);
-        setUserId(id);
-        setUserToken(token);
+        id = await createUser(generateRandomString(8), externalId, baseURL)
+        token = await createUserToken(id, baseURL)
+        await AsyncStorage.setItem('user-id', id)
+        await AsyncStorage.setItem('user-token', token)
+        setUserId(id)
+        setUserToken(token)
       }
 
       // set config
@@ -91,71 +64,126 @@ export default function ExampleApp() {
         userId: id,
         environment,
         workPlatformId,
-      };
+      }
 
-      const phylloConnect = PhylloConnect.initialize(config);
-      phylloConnect.on("exit", onExitCallBack);
-      phylloConnect.on("tokenExpired", onTokenExpiredCallBack);
-      phylloConnect.on("accountConnected", onAccountConnectedCallBack);
-      phylloConnect.on("accountDisconnected", onAccountDisconnectedCallBack);
-      phylloConnect.on("connectionFailure", onConnectionFailure);
+      PhylloConnect.initialize(config)
+      const eventEmitter = new NativeEventEmitter(PhylloConnect)
+      
+      this.eventListener = eventEmitter.addListener(
+        'onAccountConnected',
+        (event) => {
+          console.log(
+            `onAccountConnected accountId: ${event.accountId}, workplatformId: ${event.accountId}, userId: ${event.userId}`
+          )
+          Toast.show(
+            `onAccountConnected accountId: ${event.accountId}, workplatformId: ${event.workplatformId}, userId: ${event.userId}`
+          )
+          this.eventListener.remove()
+        }
+      )
+      
 
-      phylloConnect.open();
+      this.eventListener = eventEmitter.addListener(
+        'onAccountDisconnected',
+        (event) => {
+          console.log(
+            `onAccountDisconnectedCallBack accountId: ${event.accountId}, workplatformId: ${event.workplatformId}, userId: ${event.userId}`
+          )
+          this.eventListener.remove()
+        }
+      )
+      
 
-      console.log("Version Object", phylloConnect.version());
+      this.eventListener = eventEmitter.addListener(
+        'onConnectionFailure',
+        (event) => {
+          console.log(
+            `onConnectionFailure reason: ${event.reason}, workplatformId: ${event.workplatformId}, userId: ${event.userId}`
+          )
+          Toast.show(
+            `onConnectionFailure reason: ${event.reason}, workplatformId: ${event.workplatformId}, userId: ${event.userId}`
+          )
+          this.eventListener.remove()
+        }
+      )
+      
+
+      this.eventListener = eventEmitter.addListener(
+        'onTokenExpired',
+        (event) => {
+          console.log(`onTokenExpired userId: ${event.userId}`)
+          Toast.show(`onTokenExpired userId: ${event.userId}`)
+          this.eventListener.remove()
+        }
+      )
+      
+
+      this.eventListener = eventEmitter.addListener(
+        'onExit',
+        (event) => {
+          console.log(`onExit reason: ${event.reason}, userId: ${event.userId}`)
+          Toast.show(`onExit reason: ${event.reason}, userId: ${event.userId}`)
+          this.eventListener.remove()
+        }
+      )
+      
+
+      PhylloConnect.open()
+
+      //console.log('Version Object', PhylloConnect.version())
     } catch (e) {
-      Toast.show(e.message);
+      Toast.show(e.message)
     }
-  };
+  }
   return (
     <View style={styles.containerStyle}>
       <TouchableOpacity
-        onPress={() => onPressButton("")}
+        onPress={() => onPressButton('')}
         style={styles.buttonStyle}
       >
         <Text style={styles.buttonText}>Connect Platform Account(s)</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        onPress={() => onPressButton("9bb8913b-ddd9-430b-a66a-d74d846e6c66")}
+        onPress={() => onPressButton('9bb8913b-ddd9-430b-a66a-d74d846e6c66')}
         style={styles.buttonStyle}
       >
         <Text style={styles.buttonText}>Connect Instagram using Phyllo</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        onPress={() => onPressButton("14d9ddf5-51c6-415e-bde6-f8ed36ad7054")}
+        onPress={() => onPressButton('14d9ddf5-51c6-415e-bde6-f8ed36ad7054')}
         style={styles.buttonStyle}
-        underlayColor="#fff"
+        underlayColor='#fff'
       >
         <Text style={styles.buttonText}>Connect YouTube using Phyllo</Text>
       </TouchableOpacity>
 
       <BouncyCheckbox
-        fillColor="green"
+        fillColor='green'
         onPress={(isChecked) => {
-          setExistingUser(isChecked);
+          setExistingUser(isChecked)
         }}
-        text="Existing user"
+        text='Existing user'
         isChecked={existingUser}
         textStyle={{
-          textDecorationLine: "none",
+          textDecorationLine: 'none',
         }}
         style={styles.checkboxStyle}
         disabled={!userId || !userToken}
       />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   buttonStyle: {
     marginVertical: 5,
-    backgroundColor: "#524FA1",
-    borderColor: "#524FA1",
+    backgroundColor: '#524FA1',
+    borderColor: '#524FA1',
     height: 40,
-    display: "flex",
-    justifyContent: "center",
+    display: 'flex',
+    justifyContent: 'center',
     borderRadius: 5,
-    shadowColor: "black",
+    shadowColor: 'black',
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 5,
@@ -163,17 +191,17 @@ const styles = StyleSheet.create({
   },
 
   buttonText: {
-    color: "#fff",
-    textAlign: "center",
-    fontWeight: "bold",
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
   containerStyle: {
-    display: "flex",
+    display: 'flex',
     flex: 0.8,
     marginHorizontal: 35,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   checkboxStyle: {
     marginTop: 20,
   },
-});
+})
--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
-import { PhylloEnvironment } from "react-native-phyllo-connect";
-
-const clientId = "<client id here>"; // add your Id here
-const clientSecret = "<client secret here>"; // add your client Secret
-const env = PhylloEnvironment.sandbox; // add your environment type, sandbox, production are the valid values
+const clientId = '<clientId>' // add your Id here
+const clientSecret = '<client Secret>' // add your client Secret
+const env = "sandbox" // add your environment type, sandbox, production are the valid values
 
 export default {
   clientId,
   clientSecret,
   env,
-};
+}
